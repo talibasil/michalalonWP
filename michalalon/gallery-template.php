a79f29665165933d0fe5319b3927d8837ca916c4
טלי בסיל <tali-b@tdror.local>
2016-09-08 13:29:39 +0300

    all today jpb

--- michalalon/gallery-template.php
+++ michalalon/gallery-template.php
@@ -9,20 +9,20 @@
 
 get_header(); ?>
 
-	<?php $categories = get_categories();fdsfdsf
- 		
+		<?php $categories = get_categories();
+	 		
+		 		if ( ! empty( $categories ) ) {
+					foreach($categories as $category) { 
+								if (function_exists('z_taxonomy_image_url')){?>
+							<a href="<?php echo get_category_link($category->term_id); ?>">
+								<img src="<?php echo z_taxonomy_image_url($category->term_id); ?>" />
+							</a>
+						<?php }
+				    	echo esc_html( $category->name );   
+					}
+				}?>
+	  </div>
 
- 		if ( ! empty( $categories ) ) {
-			foreach($categories as $category) { 
-
-				if (function_exists('z_taxonomy_image_url')){?>
-					<a href="<?php echo get_category_link($category->term_id); ?>">
-						<img src="<?php echo z_taxonomy_image_url($category->term_id); ?>" />
-					</a>
-				<?php }
-		    	echo esc_html( $category->name );   
-			}
-		}?>
 
  
 <?php
--- michalalon/page.php
+++ michalalon/page.php
@@ -13,14 +13,14 @@
  */
 
 get_header(); ?>
-dgfdg
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 			<?php
 			while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/content', 'page' );
+				the_content();
 
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
@@ -34,5 +34,5 @@ dgfdg
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+// get_sidebar();
 get_footer();

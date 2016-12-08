170c495cef6b5babca7615396c2f75a39600e7de
טלי בסיל <tali-b@tdror.local>
2016-12-08 11:24:14 +0000

    Revert "home page slider- photo size auto"
    This reverts commit 72322e3ba5796f5a2db470999b59822eb695fe9b.
    
    :'/
    :wq

--- michalalon/js/myscript.js
+++ michalalon/js/myscript.js
@@ -1,8 +1,38 @@
 jQuery(function ($) {
 
+<<<<<<< HEAD
 	$('.accordion').height(450).shadeAccordion('.item', 450);
 	$('.grid').isotope({
 	  layoutMode: 'fitRows',
 	  itemSelector: '.imgcat'
 	});
+=======
+	if ($('.accordion').length) { //if we have slider
+
+		var heightforestscreen = $(window).height() - $('header').height() - $('footer').height();
+
+		var counter = 0,
+			end = $('.accordion').find('img').length;
+		$('.accordion').find('img').load(function() { //fires after all images loads
+		    counter += 1;
+		    counter === end && $('.accordion').height(heightforestscreen).shadeAccordion('.item', heightforestscreen);
+		}).each(function() {
+		  this.complete && $(this).load();        
+		});		
+	}
+
+	if ($('.grid').length) { //if we have slider
+		var counter = 0,
+			end = $('.grid').find('img').length;
+		$('.grid').find('img').load(function() { //fires after all images loads
+		    counter += 1;
+		    counter === end && $('.grid').isotope({layoutMode: 'fitRows', itemSelector: '.imgcat'});
+		}).each(function() {
+		  this.complete && $(this).load();        
+		});		
+	}
+<<<<<<< HEAD
+>>>>>>> parent of 72322e3... home page slider- photo size auto
+=======
+>>>>>>> parent of 72322e3... home page slider- photo size auto
 });
